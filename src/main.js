081d290b7415d572ed8e96d20b354926d7e7c870
aool <986208766@qq.com>
2020-03-06 07:06:55 +0800

    denglu

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
 import './plugins/element.js'
+import './assets/css/global.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('tolen')
+  return config
+})
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
